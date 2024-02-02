d8c21c554d566803745f8f4aa7d36b5e69cc3f21
Martin Novák <xnovm288@studenti.czu.cz>
2024-02-02 20:02:43 +0100

    zda se že toto je správný postup pro WPF

--- praktická část/HlavniUzel/HlavniUzel/App.xaml.cs
+++ praktická část/HlavniUzel/HlavniUzel/App.xaml.cs
@@ -6,25 +6,38 @@ using System.Data;
 using System.Windows;
 
 namespace HlavniUzel
-{ 
+{
     public partial class App : Application
     {
-        IHost _host;
+        private readonly IHost _host;
         public App()
         {
-           HostApplicationBuilder builder = Host.CreateApplicationBuilder();
-            builder.Services.AddServices();
-            builder.Services.AddSingleton<MainWindow>();
-
-            _host = builder.Build();           
+            _host = Host.CreateDefaultBuilder()
+                 .ConfigureServices((context, services) =>
+                 {
+                     services.AddServices();
+                     services.AddSingleton<MainWindow>();
+                 })
+                 .Build();
         }
 
         protected override async void OnStartup(StartupEventArgs e)
         {
+            await _host!.StartAsync();
+
             var startupForm = _host.Services.GetRequiredService<MainWindow>();
             startupForm.Show();
 
             base.OnStartup(e);
+        }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync();
+            }
+
+            base.OnExit(e);
         }
     }
 
