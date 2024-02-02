694efce1922061f2a5fec2f4d1ab770018d9ffd8
Martin Novák <xnovm288@studenti.czu.cz>
2024-02-02 17:18:16 +0100

    Convert EndPointDto to EndPointDo

--- praktická část/HlavniUzel/HlavniUzel.Komunikace/Dto/ValuesDto.cs
+++ praktická část/HlavniUzel/HlavniUzel.Komunikace/Dto/ValuesDto.cs
@@ -2,8 +2,8 @@
 {
     public class ValuesDto
     {
-        public int[] Ints { get; set; }
-        public float[] Floats { get; set; }
-        public bool[] Bools { get; set; }
+        public int[] Ints { get; set; }=new int[0];
+        public float[] Floats { get; set; } = new float[0];
+        public bool[] Bools { get; set; } = new bool[0];
     }
 }
--- praktická část/HlavniUzel/HlavniUzel.Logika/Do/valuesDo.cs
+++ praktická část/HlavniUzel/HlavniUzel.Logika/Do/valuesDo.cs
@@ -2,8 +2,8 @@
 {
     public class valuesDo
     {
-        public ValueDo<int>[] Ints { get; set; }
-        public ValueDo<float>[] Floats { get; set; }
-        public ValueDo<bool>[] Bools { get; set; }
+        public ValueDo<int>[] Ints { get; set; }=new ValueDo<int>[0];
+        public ValueDo<float>[] Floats { get; set; } = new ValueDo<float>[0];
+        public ValueDo<bool>[] Bools { get; set; }=new ValueDo<bool>[0];
     }
 }
