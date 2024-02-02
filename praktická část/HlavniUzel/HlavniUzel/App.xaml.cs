109e3357e2508d30fed8bdd3ff4a90d02902ecc9
Martin Novák <xnovm288@studenti.czu.cz>
2024-02-02 18:58:47 +0100

    uspesne spusteni

--- praktická část/HlavniUzel/HlavniUzel/App.xaml.cs
+++ praktická část/HlavniUzel/HlavniUzel/App.xaml.cs
@@ -1,14 +1,31 @@
-﻿using System.Configuration;
+﻿using HlavniUzel.Extentions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace HlavniUzel
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+{ 
     public partial class App : Application
     {
+        IHost _host;
+        public App()
+        {
+           HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+            builder.Services.AddServices();
+            builder.Services.AddSingleton<MainWindow>();
+
+            _host = builder.Build();           
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var startupForm = _host.Services.GetRequiredService<MainWindow>();
+            startupForm.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
