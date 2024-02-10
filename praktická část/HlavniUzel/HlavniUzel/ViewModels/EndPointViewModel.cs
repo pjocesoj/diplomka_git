145ce7433372e56f1ae299dc124e2d9f51b87b27
Martin Novák <xnovm288@studenti.czu.cz>
2024-02-10 17:55:46 +0100

    ValueDo.ToString()

---
+++ praktická část/HlavniUzel/HlavniUzel/ViewModels/EndPointViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HlavniUzel.Logika.Do;
+
+namespace HlavniUzel.ViewModels
+{
+    public partial class EndPointViewModel:ObservableObject
+    {
+        private readonly EndPointDo _endPoint;
+
+        public EndPointViewModel(EndPointDo endPoint)
+        {
+            this._endPoint = endPoint;
+
+            Address = endPoint.URL;
+        }
+
+        [ObservableProperty]
+        private string _address = "";
+    }
+}
