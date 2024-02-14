e8d62ba025cbc19d2e75b71e5585fff3eb882b0a
Martin Novák <xnovm288@studenti.czu.cz>
2024-02-14 18:53:43 +0100

    ziskání init hodnot

--- praktická část/NodeEmulator/NodeEmulator/Endpoint.cs
+++ praktická část/NodeEmulator/NodeEmulator/Endpoint.cs
@@ -8,7 +8,23 @@ namespace NodeEmulator
     {
         public EndPointDto Info { get; set; }
 
-        public ValuesDto Values { get; set; }
+        public ValuesDto Values
+        {
+            get
+            {
+                var ints = Info.Vals.Where(x => x.Type == ValType.INT).Select(x => (ValueDo<int>)x);
+                var floats = Info.Vals.Where(x => x.Type == ValType.FLOAT).Select(x => (ValueDo<float>)x);
+                var bools = Info.Vals.Where(x => x.Type == ValType.BOOL).Select(x => (ValueDo<bool>)x);
+
+                var i = ints.Select(x=>x.Value);
+                return new ValuesDto()
+                {
+                    Ints = ints.Select(x => x.Value).ToArray(),
+                    Floats = floats.Select(x => x.Value).ToArray(),
+                    Bools = bools.Select(x => x.Value).ToArray(),
+                };
+            }
+        }
 
         public Endpoint(HttpMethodEnum http, string url, ValueDto[] vals)
         {
@@ -19,7 +35,7 @@ namespace NodeEmulator
                 Vals = vals
             };
 
-            foreach (var val in vals) 
+            foreach (var val in vals)
             {
                 //var wtf = (val as ValueDo);
                 //Values.Add(wtf.Value); 
