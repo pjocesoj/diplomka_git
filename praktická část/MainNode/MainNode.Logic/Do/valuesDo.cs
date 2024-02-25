3c15f269b034ffe01907d4074a3e4eab7aa3ba72
Martin Novák <xnovm288@studenti.czu.cz>
2024-02-25 18:58:25 +0100

    vypis argumentů

--- praktická část/MainNode/MainNode.Logic/Do/valuesDo.cs
+++ praktická část/MainNode/MainNode.Logic/Do/valuesDo.cs
@@ -2,8 +2,16 @@
 {
     public class ValuesDo
     {
-        public List<ValueDo<int>> Ints { get; set; }=new List<ValueDo<int>>();
+        public List<ValueDo<int>> Ints { get; set; } = new List<ValueDo<int>>();
         public List<ValueDo<float>> Floats { get; set; } = new List<ValueDo<float>>();
-        public List<ValueDo<bool>> Bools { get; set; }=new List<ValueDo<bool>>();
+        public List<ValueDo<bool>> Bools { get; set; } = new List<ValueDo<bool>>();
+
+        public List<string> ToStringListShort()
+        {
+            var i = Ints.Select(x => x.ToStringShort());
+            var f = Floats.Select(x => x.ToStringShort());
+            var b = Bools.Select(x => x.ToStringShort());
+            return i.Concat(f).Concat(b).ToList();
+        }
     }
 }
