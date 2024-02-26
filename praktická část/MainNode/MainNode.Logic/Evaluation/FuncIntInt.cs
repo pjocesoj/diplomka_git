5292437da23138c657fe786787d33c7c282bcfa7
Martin Novák <xnovm288@studenti.czu.cz>
2024-02-26 10:04:59 +0100

    zkopírováno z proof of concept

---
+++ praktická část/MainNode/MainNode.Logic/Evaluation/FuncIntInt.cs
@@ -0,0 +1,10 @@
+﻿namespace MainNode.Logic.Evaluation
+{
+    public static class FuncIntInt
+    {
+        public static int Plus(int a, int b) { return a + b; }
+        public static int Minus(int a, int b) { return a - b; }
+        public static int Multiply(int a, int b) { return a * b; }
+        public static int devide(int a, int b) { return a / b; }
+    }
+}
---
+++ praktická část/MainNode/MainNode.Logic/Evaluation/Operation.cs
@@ -0,0 +1,28 @@
+﻿using MainNode.Logic.Do;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MainNode.Logic.Evaluation
+{
+    public class Operation<T>
+    {
+        public ValueDo<T>? Ref { get; set; }
+        public T? Const { get; set; }
+        public Func<T, T, T> Func { get; set; }
+
+        public Operation(ValueDo<T> val, Func<T, T, T> func)
+        {
+            Ref = val;
+            Func = func;
+        }
+        public Operation(T val, Func<T, T, T> func)
+        {
+            Const = val;
+            Func = func;
+        }
+        public T Execute(T b)
+        {
+            if (Ref != null) { return Func(Ref.Value, b); }
+            return Func(Const!, b);
+        }
+    }
+}
