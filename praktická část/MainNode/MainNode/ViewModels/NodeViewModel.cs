d5efa0f72f1b587436eb4890b7fd36744dacea8c
Martin Novák <xnovm288@studenti.czu.cz>
2024-03-01 22:34:50 +0100

    smazani pokusu

--- praktická část/MainNode/MainNode/ViewModels/NodeViewModel.cs
+++ praktická část/MainNode/MainNode/ViewModels/NodeViewModel.cs
@@ -53,40 +53,6 @@ namespace HlavniUzel.ViewModels
                 MessageBox.Show(ex.Message);
             }
 
-            var ep = _node.EndPoints[2];
-            List<Operation<int>> operations = new List<Operation<int>>()
-            {
-            new Operation<int>(ep.Values.Ints[0],FuncIntInt.Plus),
-            new Operation<int>(2,FuncIntInt.Multiply),
-            };
-            var instr = new Flow<int>("", operations);
-            var i=instr.Evaluate();
-            instr.Output = ep.Arguments.Ints[0];
-            instr.Run();
-
-            var re = new FlowRepository();
-            re.AddFlow(instr);
-
-            List<Operation<float>> oper2=new List<Operation<float>>()
-            {
-                new Operation<float>(2.5f,FuncFloatFloat.Plus),
-            new SubflowOperation<float,int>(instr,FuncFloatInt.devide)
-            };
-            var sub = new Flow<float>("", oper2);
-            var f = sub.Evaluate();
-            re.AddFlow(sub);
-
-            var ep0 = _node.EndPoints[0];
-            List<Operation<bool>> oper3 = new List<Operation<bool>>()
-            {
-                new RefConstOperation<bool,float>(ep0.Values.Floats[0],2,FuncBoolFloat.Greater),
-            new Operation<bool>(true,FuncBoolBool.And)
-            };
-            var rc = new Flow<bool>("", oper3);
-            var b = rc.Evaluate();
-            re.AddFlow(rc);
-
-            re.Run();
         }
     }
 }
