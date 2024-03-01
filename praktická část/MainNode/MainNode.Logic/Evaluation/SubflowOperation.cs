24d29d884be0afa95bc5dc504c4c788a9d9dd5f6
Martin Novák <xnovm288@studenti.czu.cz>
2024-03-01 19:13:57 +0100

    potomek operace

---
+++ praktická část/MainNode/MainNode.Logic/Evaluation/SubflowOperation.cs
@@ -0,0 +1,24 @@
+﻿using MainNode.Logic.Do;
+
+namespace MainNode.Logic.Evaluation
+{
+    public class SubflowOperation<T>:Operation<T> where T:struct
+    {
+        public Flow<T> Subflow { get; set; }
+        public Func<T, T, T> Func { get; set; }
+        public SubflowOperation(Flow<T> flow, Func<T, T, T> func)
+        {
+            Subflow = flow;
+            Func = func;
+        }
+        public override T Execute(T b)
+        {
+            var temp = Subflow.Evaluate();
+            if (Subflow.Output != null)
+            {
+                Subflow.Output.Value = temp;
+            }
+            return Func(temp, b);
+        }
+    }
+}
