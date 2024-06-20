b0add5523bc8ea0c876cc8ec04a80dc0094cf778
Martin Novák <xnovm288@studenti.czu.cz>
2024-06-20 19:35:20 +0200

    SharedHttpEndpoints.cpp

---
+++ praktická část/ESP/SharedHttpEndpoints.cpp
@@ -0,0 +1,57 @@
+#include "SharedHttpEndpoints.h"
+
+#include "ESP8266WebServer.h"
+#include "global.h"
+
+void getInfo()
+{
+    DynamicJsonDocument doc(1024);
+    JsonArray EPs = doc.to<JsonArray>();
+    for (auto &obj : endpoints)
+    {
+        JsonObject nestedJsonObject = EPs.createNestedObject();
+        obj->Serialize_info(nestedJsonObject);
+    }
+
+    String ret;
+    serializeJson(doc, ret);
+    Serial.println(ret);
+    server.send(200, "text/plain", ret);
+}
+
+void handleRootPath()
+{
+    Serial.println("http root");
+    int headers = server.headers();
+    for (int i = 0; i < headers; i++)
+    {
+        String val = server.header(i);
+        String nam = server.headerName(i);
+        Serial.print(nam);
+        Serial.print("=");
+        Serial.println(val);
+    }
+
+    String body = server.arg("plain");
+    Serial.println(body);
+
+    server.send(200, "text/plain", "Hello world");
+}
+
+void AddDefaultEndpoints()
+{
+    server.on("/", handleRootPath);
+    server.on("/getInfo", getInfo);
+}
+
+void sendEndpointValues(Endpoint *e)
+{
+    DynamicJsonDocument doc(1024);
+    JsonObject jsonObject = doc.to<JsonObject>();
+    e->Serialize_values(jsonObject);
+
+    String ret;
+    serializeJson(doc, ret);
+    Serial.println(ret);
+    server.send(200, "text/plain", ret);
+}
--- praktická část/ESP/SharedHttpEndpoints.h
+++ praktická část/ESP/SharedHttpEndpoints.h
@@ -1,61 +1,11 @@
 #ifndef SHARED_HTTP_H_
 #define SHARED_HTTP_H_
 
-#include "ESP8266WebServer.h"
-#include "global.h"
+#include "EndPoint.h"
 
-//ESP8266WebServer server(80);
-void getInfo()
-{
-  DynamicJsonDocument doc(1024);
-  JsonArray EPs = doc.to<JsonArray>();
-  for (auto &obj : endpoints)
-  {
-    JsonObject nestedJsonObject = EPs.createNestedObject();
-    obj->Serialize_info(nestedJsonObject);
-  }
-
-  String ret;
-  serializeJson(doc, ret);
-  Serial.println(ret);
-  server.send(200, "text/plain", ret);
-}
-
-void handleRootPath()
-{
-  Serial.println("http root");
-  int headers = server.headers();
-  for (int i = 0; i < headers; i++)
-  {
-    String val = server.header(i);
-    String nam = server.headerName(i);
-    Serial.print(nam);
-    Serial.print("=");
-    Serial.println(val);
-  }
-
-  String body = server.arg("plain");
-  Serial.println(body);
-
-  server.send(200, "text/plain", "Hello world");
-}
-
-void AddDefaultEndpoints()
-{
-  server.on("/", handleRootPath);
-  server.on("/getInfo", getInfo);
-}
-
-void sendEndpointValues(Endpoint *e)
-{
-  DynamicJsonDocument doc(1024);
-  JsonObject jsonObject = doc.to<JsonObject>();
-  e->Serialize_values(jsonObject);
-
-  String ret;
-  serializeJson(doc, ret);
-  Serial.println(ret);
-  server.send(200, "text/plain", ret);
-}
+void getInfo();
+void handleRootPath();
+void AddDefaultEndpoints();
+void sendEndpointValues(Endpoint *e);
 
 #endif
