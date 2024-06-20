30d68d331064fcf36cca8c7748e9632147579193
Martin Novák <xnovm288@studenti.czu.cz>
2024-06-20 19:29:21 +0200

    helpers.cpp

---
+++ praktická část/ESP/helpers.cpp
@@ -0,0 +1,48 @@
+#include "helpers.h"
+
+#include "HardwareSerial.h"
+#include "Esp.h"
+
+void AvailableRAM(char* const title)
+{
+  Serial.print(title);
+
+  Serial.print("\t Free heap: ");
+  Serial.print(ESP.getFreeHeap());
+
+  Serial.print("\t stack: ");
+  Serial.println(ESP.getFreeContStack());
+}
+
+void deserializace(String json, Endpoint *ep)
+{
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, json);
+
+  JsonArray array1 = doc["Ints"];
+  int j=0;
+  for(int i : array1)   
+  {
+    ep->Ints[j]->Value=i;
+    Serial.println(i);
+    j++;
+  }
+
+  JsonArray array2 = doc["Floats"];
+  j=0;
+  for(float i : array2) 
+  {
+    ep->Floats[j]->Value=i;
+    Serial.println(i);
+    j++;
+  }
+
+  JsonArray array3 = doc["Bools"];
+  j=0;
+  for(bool i : array3) 
+  {
+    ep->Bools[j]->Value=i;
+    Serial.println(i);
+    j++;
+  }
+}
