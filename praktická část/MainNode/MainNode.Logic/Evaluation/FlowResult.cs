9e6d8a6ed6bbdbd7098054ba8a5a09af136726ad
Martin Novák <xnovm288@studenti.czu.cz>
2024-08-04 18:36:05 +0200

    FlowResult

---
+++ praktická část/MainNode/MainNode.Logic/Evaluation/FlowResult.cs
@@ -0,0 +1,35 @@
+﻿using MainNode.Logic.Do;
+
+namespace MainNode.Logic.Evaluation
+{
+    /// <summary>
+    /// obsahuje výsledek flow a informaci zda je aktuální<br/>
+    /// pokud ne tak se flow spustí a vrátí výsledek
+    /// </summary>
+    public class FlowResult<T> where T : struct
+    {
+        public bool Finished { get; set; } = false;
+        public Flow<T> Flow { get; set; }
+
+        private ValueDo<T> _valueDo;
+        public FlowResult(Flow<T> flow)
+        {           
+            Flow = flow;
+            _valueDo = new ValueDo<T>($"{flow.Name}_out",default);
+            Flow.Output = _valueDo;
+        }
+
+        public ValueDo<T> Value 
+        {
+            get
+            {
+                if(Finished) { return _valueDo; }
+                
+                Flow.Run();
+                //_valueDo.Value = Flow.Output.Value;
+                Finished = true;
+                return _valueDo;
+            }
+        }
+    }
+}
