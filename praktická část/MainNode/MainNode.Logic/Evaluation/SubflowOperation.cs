490109dc60c1c7bd218740c2657eae839c39fb5f
Martin Novák <xnovm288@studenti.czu.cz>
2024-08-04 19:07:19 +0200

    subflow rework

--- praktická část/MainNode/MainNode.Logic/Evaluation/SubflowOperation.cs
+++ praktická část/MainNode/MainNode.Logic/Evaluation/SubflowOperation.cs
@@ -2,23 +2,19 @@
 
 namespace MainNode.Logic.Evaluation
 {
-    public class SubflowOperation<T,U>:Operation<T> where T:struct where U : struct
+    public class SubflowOperation<T, U> : Operation<T> where T : struct where U : struct
     {
-        public Flow<U> Subflow { get; set; }
+        public FlowResult<U> Subflow { get; set; }
         public new Func<U, T, T> Func { get; set; }
-        public SubflowOperation(Flow<U> flow, Func<U, T, T> func)
+        public SubflowOperation(FlowResult<U> flow, Func<U, T, T> func)
         {
             Subflow = flow;
             Func = func;
         }
         public override T Execute(T b)
         {
-            var temp = Subflow.Evaluate();
-            if (Subflow.Output != null)
-            {
-                Subflow.Output.Value = temp;
-            }
-            return Func(temp, b);
+            var temp = Subflow.Value;
+            return Func(temp.Value, b);
         }
     }
 }
