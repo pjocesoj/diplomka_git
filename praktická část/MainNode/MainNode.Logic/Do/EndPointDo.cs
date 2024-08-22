0ef433f82fcfe85ff6a5a84829fe64376e31d2d4
Martin Novák <xnovm288@studenti.czu.cz>
2024-08-22 19:41:26 +0200

    working with bugs

--- praktická část/MainNode/MainNode.Logic/Do/EndPointDo.cs
+++ praktická část/MainNode/MainNode.Logic/Do/EndPointDo.cs
@@ -7,11 +7,6 @@ namespace MainNode.Logic.Do
     {
         public EndPointPath Path { get; set; }
         public EndpointType Type { get; set; }
-        /*
-        public List<ValueDo<int>> Ints { get; } = new List<ValueDo<int>>();
-        public List<ValueDo<float>> Flots { get; } = new List<ValueDo<float>>();
-        public List<ValueDo<bool>> Bools { get; } = new List<ValueDo<bool>>();
-        */
         public ValuesDo Values { get; set; }
         public ValuesDo Arguments { get; set; }
         public int? Delay { get; set; }
