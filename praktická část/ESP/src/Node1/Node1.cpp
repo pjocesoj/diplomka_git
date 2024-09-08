b764e346c1d786d3e7e5a30fe3157b2e49ddbc05
Martin Novák <xnovm288@studenti.czu.cz>
2024-09-08 21:33:40 +0200

    cleaning

--- praktická část/ESP/src/Node1/Node1.cpp
+++ praktická část/ESP/src/Node1/Node1.cpp
@@ -13,18 +13,36 @@
 
 DhtWrapper _dhtWrapper(D4);
 
-Endpoint* _dhtNew;
-Endpoint* _dhtAny;
+Endpoint *_dhtNew;
+Endpoint *_dhtAny;
+
+void DhtPrint(float temp, float humid)
+{
+	clearDisplay(0, 21);
+	ShowText(temp, 2, 0, 21);
+	ShowDeg(2);
+	ShowText("C", 2);
+	newLine();
+	ShowText(humid, 2);
+	ShowText(" %", 2);
+	ShowText(MillisToTimestemp(millis()), 1, 0, 50);
+}
 
 void getDhtValuesNew()
 {
 	_dhtWrapper.WaitForNewestData();
 
-	_dhtNew->Floats[0]->Value = _dhtWrapper.GetTemp();
-	_dhtNew->Floats[1]->Value = _dhtWrapper.GetHumid();
-	_dhtNew->Ints[0]->Value = _dhtWrapper.GetDataAge();
+	float temp = _dhtWrapper.GetTemp();
+	float humid = _dhtWrapper.GetHumid();
+	long age = _dhtWrapper.GetDataAge();
+
+	_dhtNew->Floats[0]->Value = temp;
+	_dhtNew->Floats[1]->Value = humid;
+	_dhtNew->Ints[0]->Value = age;
 
 	sendEndpointValues(_dhtNew);
+
+	DhtPrint(temp, humid);
 }
 void getDhtValuesAny()
 {
@@ -40,16 +58,9 @@ void getDhtValuesAny()
 
 	sendEndpointValues(_dhtAny);
 
-	clearDisplay(0,21);
-	ShowText(temp,2,0,21);
-	ShowDeg(2);
-	ShowText("C",2);
-	newLine();
-	ShowText(humid,2);
-	ShowText(" %",2);
-	ShowText(MillisToTimestemp(millis()),1,0,50);
+	DhtPrint(temp, humid);
 }
-Endpoint* create_getDhtNew()
+Endpoint *create_getDhtNew()
 {
 	_dhtNew = new Endpoint(GET, "/getDhtValuesNew");
 	_dhtNew->Floats.push_back(new ValueDto<float>("temp", 0));
@@ -61,7 +72,7 @@ Endpoint* create_getDhtNew()
 	server.on(_dhtNew->URL, getDhtValuesNew);
 	return _dhtNew;
 }
-Endpoint* create_getDhtAny()
+Endpoint *create_getDhtAny()
 {
 	_dhtAny = new Endpoint(GET, "/getDhtValuesAny");
 	_dhtAny->Floats.push_back(new ValueDto<float>("temp", 0));
@@ -74,60 +85,14 @@ Endpoint* create_getDhtAny()
 	return _dhtAny;
 }
 
-void getValues()
-{
-	sendEndpointValues(endpoints[0]);
-}
-
-Endpoint* test_get()
-{
-	Endpoint* e1 = new Endpoint(GET, "/getValues");
-	e1->Ints.push_back(new ValueDto<int>("a", 1));
-	e1->Ints.push_back(new ValueDto<int>("b", 2));
-	e1->Floats.push_back(new ValueDto<float>("c", 3.14));
-	e1->Bools.push_back(new ValueDto<bool>("B1", true));
-	endpoints.push_back(e1);
-
-	server.on(e1->URL, getValues);
-	return e1;
-}
-
-void setValues()
-{
-	Serial.println("setValue");
-	String body = server.arg("plain");
-	deserializeDTO(body, endpoints[0]);
-
-	server.send(200, "text/plain", "ok");
-}
-
-Endpoint* test_set()
-{
-	Endpoint* e2 = new Endpoint(POST, "/setValues");
-	e2->Ints.push_back(new ValueDto<int>("a", 1));
-	e2->Ints.push_back(new ValueDto<int>("b", 2));
-	e2->Floats.push_back(new ValueDto<float>("c", 3.14));
-	e2->Bools.push_back(new ValueDto<bool>("B1", true));
-	endpoints.push_back(e2);
-
-	server.on(e2->URL, setValues);
-	return e2;
-}
-
-
-
 void NodeInit()
 {
 	Serial.println("NODE 1");
 
-	Endpoint* e1 = test_get();
+	Endpoint *e1 = create_getDhtNew();
 	printEndpoint(e1);
-	Endpoint* e2 = test_set();
+	Endpoint *e2 = create_getDhtAny();
 	printEndpoint(e2);
-	Endpoint* e3 = create_getDhtNew();
-	printEndpoint(e3);
-	Endpoint* e4 = create_getDhtAny();
-	printEndpoint(e4);
 
 	bool oled = OledInit();
 	if (!oled)
