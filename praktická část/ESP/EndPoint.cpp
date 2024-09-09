86150ae6ca356bfe684a4a26fa9f63b4468f8392
Martin Novák <xnovm288@studenti.czu.cz>
2024-09-09 17:17:54 +0200

    update Values serialization

--- praktická část/ESP/EndPoint.cpp
+++ praktická část/ESP/EndPoint.cpp
@@ -97,28 +97,37 @@ void Endpoint::Serialize_info(JsonObject &jsonObject)
  */
 void Endpoint::Serialize_values(JsonObject &jsonObject)
 {
-  // JsonArray ints = jsonObject.createNestedArray("Ints");
+  // JsonArray ints = jsonObject.createNestedArray("Ints"); //ArduinoJson 6.x
   JsonArray ints = jsonObject["Ints"].to<JsonArray>();
   for (auto &obj : Ints)
   {
-    // JsonObject nestedJsonObject = ints.createNestedObject();
-    JsonObject nestedJsonObject = ints.add<JsonObject>();
-    obj->Serialize_value(nestedJsonObject);
+    // JsonObject nestedJsonObject = ints.createNestedObject(); //ArduinoJson 6.x
+    
+    //JsonObject nestedJsonObject = ints.add<JsonObject>(); //ArduinoJson 7.1
+    //obj->Serialize_value(nestedJsonObject);
+
+    ints.add(obj->Value);
   }
-  // JsonArray floats = jsonObject.createNestedArray("Floats");
+  // JsonArray floats = jsonObject.createNestedArray("Floats"); //ArduinoJson 6.x
   JsonArray floats = jsonObject["Floats"].to<JsonArray>();
   for (auto &obj : Floats)
   {
-    // JsonObject nestedJsonObject = floats.createNestedObject();
-    JsonObject nestedJsonObject = floats.add<JsonObject>();
-    obj->Serialize_value(nestedJsonObject);
+    // JsonObject nestedJsonObject = floats.createNestedObject(); //ArduinoJson 6.x
+    
+    //JsonObject nestedJsonObject = floats.add<JsonObject>(); //ArduinoJson 7.1
+    //obj->Serialize_value(nestedJsonObject);
+
+    floats.add(obj->Value);
   }
-  // JsonArray bools = jsonObject.createNestedArray("Bools");
+  // JsonArray bools = jsonObject.createNestedArray("Bools"); //ArduinoJson 6.x
   JsonArray bools = jsonObject["Bools"].to<JsonArray>();
   for (auto &obj : Bools)
   {
-    // JsonObject nestedJsonObject = bools.createNestedObject();
-    JsonObject nestedJsonObject = bools.add<JsonObject>();
-    obj->Serialize_value(nestedJsonObject);
+    // JsonObject nestedJsonObject = bools.createNestedObject(); //ArduinoJson 6.x
+    
+    //JsonObject nestedJsonObject = bools.add<JsonObject>(); //ArduinoJson 7.1
+    //obj->Serialize_value(nestedJsonObject);
+
+    bools.add(obj->Value);
   }
 }
