1fdb034bfaf8a5781913343135fb0fcc5cb641a9
Martin Novák <xnovm288@studenti.czu.cz>
2024-11-01 13:26:13 +0100

    mock node

---
+++ praktická část/MainNode/Tests/MainNode.Logic.Test/Compile/LoopCompilerNodeTests.IntIntInt.cs
@@ -0,0 +1,22 @@
+﻿namespace MainNode.Logic.Test.Compile
+{
+    public partial class LoopCompilerNodeTests
+    {
+        [TestMethod]
+        public void III_Plus_NodeConst()
+        {
+            _mockNode.Get_a.Value = 1;
+            _loopCompiler.Compile("mock.setValues.a=mock.getValuesG.a+1");
+
+            // Act
+            var a0 = _mockNode.Set_a;
+            _flowRepo.Run();
+            var a1 = _mockNode.Set_a;
+            _flowRepo.Run();
+            var a2 = _mockNode.Set_a;
+            // Assert
+            Assert.AreEqual(2, a1.Value);
+            Assert.AreEqual(3, a2.Value);
+        }
+    }
+}
---
+++ praktická část/MainNode/Tests/MainNode.Logic.Test/Compile/LoopCompilerNodeTests.cs
@@ -0,0 +1,27 @@
+﻿using MainNode.Logic.Compile;
+using MainNode.Logic.Evaluation.Funcs;
+
+namespace MainNode.Logic.Test.Compile
+{
+    [TestClass]
+    public partial class LoopCompilerNodeTests
+    {
+        private LoopCompiler _loopCompiler;
+        private FlowRepository _flowRepo;
+        private NodeRepository _nodeRepo;
+        private MockNode _mockNode;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _flowRepo = new FlowRepository();
+            _nodeRepo = new NodeRepository();
+            var funcRepo = new FuncRepo();
+            _loopCompiler = new LoopCompiler(_flowRepo, _nodeRepo, funcRepo);
+
+            var comm = new Communication.HttpNodeCommunication();//dočasně než udělám mock komunikace
+            _mockNode = new MockNode(comm);
+            _nodeRepo.Nodes.Add(_mockNode.Node);
+        }
+    }
+}
