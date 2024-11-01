e570b0275a06e2cf43ffe481f1bcd3c1022ef5ec
Martin Novák <xnovm288@studenti.czu.cz>
2024-11-01 17:20:55 +0100

    hardcoded mock as test

---
+++ praktická část/MainNode/Tests/MainNode.Logic.Test/HardcodedFlow/HardcodedLoopTests.Merge.cs
@@ -0,0 +1,47 @@
+﻿using MainNode.Logic.Do;
+using MainNode.Logic.Evaluation;
+
+namespace MainNode.Logic.Test.HardcodedFlow
+{
+    public partial class HardcodedLoopTests
+    {
+        [TestMethod]
+        public void Merge()
+        {
+            var C = mergeAB(A, B);
+
+            var res = (C.Value as ValueDo<bool>).Value;
+            Assert.AreEqual(false, res);
+
+            _flowRepo.Run();
+            StartValue.Value = 4;
+            _flowRepo.Run();
+            res = (C.Value as ValueDo<bool>).Value;
+            Assert.AreEqual(true, res);
+
+            var D = addD(C);
+            res = (D.Value as ValueDo<bool>).Value;
+            Assert.AreEqual(true, res);
+        }
+        public FlowResult mergeAB(FlowResult A, FlowResult B)
+        {
+            Flow<bool> flowC = new Flow<bool>("C", new List<Operation<bool>>()
+                {
+                    new MergeflowOperation<int, float, bool>((FlowResult<int>)A, (FlowResult<float>)B, (a, b) => { return a > b; })
+            });
+            var resC = new FlowResult<bool>(flowC);
+            return _flowRepo.AddFlow(flowC);
+        }
+        public FlowResult addD(FlowResult C)
+        {
+            Flow<bool> flowD = new Flow<bool>("D", new List<Operation<bool>>()
+                {
+                    new SubflowOperation<bool,bool>((FlowResult<bool>)C,FuncBoolBool.Or),
+                    new Operation<bool>(true, FuncBoolBool.And),
+                });
+            var resD = new FlowResult<bool>(flowD);
+
+            return _flowRepo.AddFlow(flowD);
+        }
+    }
+}
---
+++ praktická část/MainNode/Tests/MainNode.Logic.Test/HardcodedFlow/HardcodedLoopTests.Subflow.cs
@@ -0,0 +1,34 @@
+﻿using MainNode.Logic.Do;
+using MainNode.Logic.Evaluation;
+
+namespace MainNode.Logic.Test.HardcodedFlow
+{
+    public partial class HardcodedLoopTests
+    {
+        [TestMethod]
+        public void Subflow()
+        {
+            var C = addC(A, B);
+
+            var res = (C.Value as ValueDo<float>).Value;
+            Assert.AreEqual(8.28, res, 0.001);//do výsledku se vkradou desetiná místa a test spadne
+
+            _flowRepo.Run();
+            StartValue.Value = 4;
+            _flowRepo.Run();
+            res = (C.Value as ValueDo<float>).Value;
+            Assert.AreEqual(14.28, res, 0.001);//do výsledku se vkradou desetiná místa a test spadne
+        }
+        public FlowResult addC(FlowResult A, FlowResult B)
+        {
+            Flow<float> flowC = new Flow<float>("C", new List<Operation<float>>()
+            {
+                new SubflowOperation<float,int>((FlowResult<int>)A,(float a,int b)=>{return a+b; }),
+                new SubflowOperation<float,float>((FlowResult<float>)B, FuncFloatFloat.Plus)
+            });
+            var resC = new FlowResult<float>(flowC);
+
+            return _flowRepo.AddFlow(flowC);
+        }
+    }
+}
