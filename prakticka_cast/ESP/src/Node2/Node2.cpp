47aa4ca460e453a323dee92c6a7ecc966075f378
Martin Novák <xnovm288@studenti.czu.cz>
2025-03-23 20:37:48 +0100

    get values

--- prakticka_cast/ESP/src/Node2/Node2.cpp
+++ prakticka_cast/ESP/src/Node2/Node2.cpp
@@ -36,7 +36,7 @@ EndPointDto *create_setAngle()
 {
     EndPointDto *e1 = new EndPointDto(POST, "/setAngle", EP_TYPE_SET);
     e1->Arg_Ints.push_back(new ValueDto<int>("angle", 0));
-    e1->Val_Ints.push_back(new ValueDto<int>("ret", 0));
+    e1->Val_Ints.push_back(new ValueDto<int>("deg", 0));
     endpoints.push_back(e1);
 
     communicationHandler.StartListening(e1->URL, SetAngle);
@@ -44,63 +44,34 @@ EndPointDto *create_setAngle()
     return e1;
 }
 
-EndPointDto* _get;
-EndPointDto* _set;
-
-void getValues()
+void GetAngle()
 {
-    sendEndpointValues(_get);
+	int ret = _servoWrapper.GetAngle();
+    _setAngle->Val_Ints[0]->Value = ret;
+
+	sendEndpointValues(_setAngle);
 }
 
-EndPointDto *test_get()
+EndPointDto *create_getAngle()
 {
-    EndPointDto *e1 = new EndPointDto(GET, "/getValues");
-    e1->Val_Ints.push_back(new ValueDto<int>("a", 10));
-    e1->Val_Ints.push_back(new ValueDto<int>("b", 20));
+    EndPointDto *e1 = new EndPointDto(GET, "/getAngle");
+    e1->Val_Ints.push_back(new ValueDto<int>("deg", 0));
     endpoints.push_back(e1);
 
-    communicationHandler.StartListening(e1->URL, getValues);
-    _get = e1;
+    communicationHandler.StartListening(e1->URL, SetAngle);
+    _getAngle = e1;
     return e1;
-}
-
-void setValues()
-{
-    
-    Serial.println("setValue");
-
-char body[512];
-    int bl = communicationHandler.GetBody(body, 512);
-    Deserialize(body, _set);
-
-    printEndpoint(_set);
-
-    communicationHandler.SendOk("ok");
-}
-EndPointDto *test_set()
-{
-    EndPointDto *e2 = new EndPointDto(POST, "/setValues", EP_TYPE_SET);
-    e2->Arg_Ints.push_back(new ValueDto<int>("a", 1));
-    e2->Arg_Ints.push_back(new ValueDto<int>("b", 2));
-    e2->Arg_Floats.push_back(new ValueDto<float>("c", 3.14));
-    e2->Arg_Bools.push_back(new ValueDto<bool>("B1", true));
-    endpoints.push_back(e2);
-    communicationHandler.StartListening(e2->URL, setValues);
-    _set = e2;
-    return e2;
 }
 
 void NodeInit()
 {
     Serial.println("NODE 2");
 
-    EndPointDto *e1 = test_get();
+    EndPointDto *e1 = create_setAngle();
     printEndpoint(e1);
-    EndPointDto *e2 = test_set();
-    printEndpoint(e2);
 
-    EndPointDto *e3 = create_setAngle();
-    printEndpoint(e3);
+    EndPointDto *e2 = create_getAngle();
+    printEndpoint(e2);
 }
 
 #endif
